c1db7346a4ce985504516ac912109759292e7e31
Vincent Delecroix <vincent.delecroix@u-bordeaux.fr>
2017-10-08 09:12:25 +0200

    remove cache from travis script

--- .travis_sage_install.sh
+++ .travis_sage_install.sh
@@ -1,20 +1,22 @@
 #!/usr/bin/env bash
 # Sage installation script for Travis-CI
 
+cd ${HOME}
+wget ${SAGE_MIRROR}/${SAGE_BINARY}
+tar xf ${SAGE_BINARY}
+mv SageMath ${SAGE_ROOT}
+ls ${SAGE_ROOT}
+
 if [ ! -x "${SAGE}" ] ;
 then
-    cd ${HOME}
-    echo "Downloading and installing Sage..."
-    wget ${SAGE_MIRROR}/${SAGE_BINARY}
-    tar xf ${SAGE_BINARY}
-    echo "done"
+    echo "Installation failed!"
+    exit 1
+fi
 
-    if [ ! -x "${SAGE}" ] ;
-    then
-        echo "Installation failed!"
-        exit 1
-    fi
-
-    # the first time we launch Sage the relocalization script is run
-    ${SAGE} -c ''
+# the first time we launch Sage the relocalization script is run
+${SAGE} -c ''
+if [ $? -ne 0 ]
+then
+    echo "Sage does not start"
+    exit 1
 fi
