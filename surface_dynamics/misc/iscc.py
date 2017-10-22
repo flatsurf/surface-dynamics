5567d36913c3c98fe1a28a963e48ddcee083275c
Vincent Delecroix <vincent.delecroix@u-bordeaux.fr>
2017-10-22 13:36:34 +0200

    fix doctests in iscc.py

--- surface_dynamics/misc/iscc.py
+++ surface_dynamics/misc/iscc.py
@@ -6,12 +6,7 @@ def iscc_rg_string(r):
         sage: from surface_dynamics.misc.iscc import iscc_rg_string
         sage: r = RibbonGraph(faces='(0,2,4,6,7)(8,9,5,3,1)', edges='(0,1)(2,3)(4,5)(6,7)(8,9)')
         sage: iscc_rg_string(r)
-        '[b0, b1] -> { [l0, l1, l2, l3, l4, l5, l6, l7, l8, l9] :
-               l0 >= 0 and l1 >= 0 and l2 >= 0 and
-               l3 >= 0 and l4 >= 0 and l5 >= 0 and
-               l6 >= 0 and l7 >= 0 and l8 >= 0 and l9 >= 0 and
-               b0 = l0 + l2 + l4 + l6 + l7 and
-               b1 = l1 + l8 + l9 + l5 + l3 }'
+        '[b0, b1] -> { [l0, l1, l2, l3, l4, l5, l6, l7, l8, l9] : l0 >= 0 and l1 >= 0 and l2 >= 0 and l3 >= 0 and l4 >= 0 and l5 >= 0 and l6 >= 0 and l7 >= 0 and l8 >= 0 and l9 >= 0 and l0 = l1 and l2 = l3 and l4 = l5 and l6 = l7 and l8 = l9 and b0 = l0 + l2 + l4 + l6 + l7 and b1 = l1 + l8 + l9 + l5 + l3 }'
     """
     boundary_vars = ', '.join('b%d'%i for i in range(r.num_faces()))
     lengths_vars = ', '.join('l%d'%i for i in r.darts())
@@ -39,6 +34,7 @@ def iscc_cd_string(cd):
     EXAMPLES::
 
         sage: from surface_dynamics.misc.iscc import iscc_cd_string
+        sage: from surface_dynamics import CylinderDiagram
         sage: cd = CylinderDiagram('(0,1)-(0,2) (2)-(1)')
         sage: iscc_cd_string(cd)
         '[w0, w1] -> { [l0, l1, l2] : l0 >= 0 and l1 >= 0 and l2 >= 0 and w0 = l0 + l1 = l0 + l2 and w1 = l2 = l1 }'
@@ -71,11 +67,11 @@ def iscc_card(arg):
         sage: from surface_dynamics import *
         sage: from surface_dynamics.misc.iscc import iscc_card
         sage: cd = CylinderDiagram('(0,1)-(0,2) (2)-(1)')
-        sage: iscc_card(cd)
+        sage: iscc_card(cd)  # optional - barvinok
         '[w0, w1] -> { [l0, l1, l2] : l0 >= 0 and l1 >= 0 and l2 >= 0 and w0 = l0 + l1 = l0 + l2 and w1 = l2 = l1 }'
 
         sage: r = RibbonGraph(faces='(0,2,4,6,7)(8,9,5,3,1)', edges='(0,1)(2,3)(4,5)(6,7)(8,9)')
-        sage: iscc_card(r)
+        sage: iscc_card(r)   # optional - barvinok
         '[b0, b1] -> { ((1 + 25/12 * b0 + 35/24 * b0^2 + 5/12 * b0^3 + 1/24 * b0^4) + (25/12 + 625/144 * b0 + 875/288 * b0^2 + 125/144 * b0^3 + 25/288 * b0^4) * b1 + (35/24 + 875/288 * b0 + 1225/576 * b0^2 + 175/288 * b0^3 + 35/576 * b0^4) * b1^2 + (5/12 + 125/144 * b0 + 175/288 * b0^2 + 25/144 * b0^3 + 5/288 * b0^4) * b1^3 + (1/24 + 25/288 * b0 + 35/576 * b0^2 + 5/288 * b0^3 + 1/576 * b0^4) * b1^4) : b0 >= 0 and b1 >= 0 }\n'
     """
     from surface_dynamics.flat_surfaces.separatrix_diagram import CylinderDiagram
