dd8c3d2185025ea864be8e8221c4c2294163e7fc
Vincent Delecroix <vincent.delecroix@u-bordeaux.fr>
2018-11-11 15:17:46 +0100

    activate warnings

--- surface_dynamics/__init__.py
+++ surface_dynamics/__init__.py
@@ -6,6 +6,10 @@ from __future__ import absolute_import
 
 from .version import version as __version__
 
+import warnings
+warnings.filterwarnings('default',
+    r'\[surface_dynamics].*')
+
 from .flat_surfaces.all import *
 from .interval_exchanges.all import *
 from .misc.constellation import Constellation, Constellations
