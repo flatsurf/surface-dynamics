94dc5cc83035da073f0d1fd4f9cccdb18fe90384
Vincent Delecroix <vincent.delecroix@u-bordeaux.fr>
2019-03-25 10:31:27 +0100

    include surface_dynamics/topology/

--- surface_dynamics/__init__.py
+++ surface_dynamics/__init__.py
@@ -10,6 +10,7 @@ import warnings
 warnings.filterwarnings('default',
     r'\[surface_dynamics].*')
 
+from .topology.all import *
 from .flat_surfaces.all import *
 from .interval_exchanges.all import *
 from .misc.constellation import Constellation, Constellations
