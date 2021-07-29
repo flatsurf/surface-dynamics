ce44cd2039f00c1bc1d48c64ed8b853742b8ac30
Vincent Delecroix <vincent.delecroix@u-bordeaux.fr>
2021-07-29 11:39:38 +0200

    remove shifted_symmetric_functions.py file added by mistake

--- surface_dynamics/misc/shifted_symmetric_functions.py
+++
@@ -1,8 +0,0 @@
-r"""
-- Kerov-Olshanski rewrite of central characters as a shifted symmetric function
-- Bloch-Okounkov bracket
-
-Shifted symmetric functions are "the same thing" as symmetric functions (as
-an algebra)
-"""
-
