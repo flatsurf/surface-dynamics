b944b4bb1349fb0eab41cf59db95e8c8f538e385
Christopher Zhang <christophertzhang@gmail.com>
2023-05-31 13:46:40 -0400

    added tests for twist dimension

--- tests/test_twist.py
+++ tests/test_twist.py
@@ -12,3 +12,11 @@ def test_find_homologous_cylinders():
     cd = CylinderDiagram('(0)-(2) (1,2,3)-(4,5) (4)-(3) (5)-(0,1)')
     tw = Twist(cd)
     assert tw.find_homologous_cylinders() == []
+
+def test_twist_dimension():
+    cd = CylinderDiagram('(0)-(1) (1,3,4,2)-(5,6) (5)-(0,4) (6)-(2,3)')
+    tw = Twist(cd)
+    assert tw.twist_dimension() == 3
+    cd = CylinderDiagram('(0,2,1)-(5,6) (3,4)-(0,2,1) (5)-(4) (6)-(3)')
+    tw = Twist(cd)
+    assert tw.twist_dimension() == 2
