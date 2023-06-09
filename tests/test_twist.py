814e6e67535f9beb9a4e7ce721e2d5fada4bbc5c
Vincent Delecroix <vincent.delecroix@u-bordeaux.fr>
2023-06-09 15:25:43 +0200

    some renaming and more accuracy

--- tests/test_twist.py
+++ tests/test_twist.py
@@ -3,7 +3,7 @@ import pytest
 from surface_dynamics import CylinderDiagram
 from surface_dynamics.flat_surfaces.twist_space import TwistSpace
 
-def test_find_homologous_cylinders():
+def test_homologous_cylinders():
     cd = CylinderDiagram('(0,2,1)-(4,5) (3,5)-(0,2,1) (4)-(3)')
     assert cd.homologous_cylinders() == [[0, 1]]
     cd = CylinderDiagram('(0,3)-(0,5) (1,2)-(1,4) (4,6)-(3,7) (5,7)-(2,6)')
@@ -11,7 +11,7 @@ def test_find_homologous_cylinders():
     cd = CylinderDiagram('(0)-(2) (1,2,3)-(4,5) (4)-(3) (5)-(0,1)')
     assert cd.homologous_cylinders() == []
 
-def test_twist_dimension():
+def test_cylinder_dimension():
     cd = CylinderDiagram('(0)-(1) (1,3,4,2)-(5,6) (5)-(0,4) (6)-(2,3)')
     tw = TwistSpace(cd)
     assert tw.cylinder_dimension() == 3
