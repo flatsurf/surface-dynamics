382d77588705f4a0a1aa296d02cfbd0d5a07d60f
Julian Rüth <julian.rueth@fsfe.org>
2024-03-15 14:31:18 +0200

    bumped version to 0.5.0

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -7,7 +7,7 @@ copyright = '2021-2023, the surface-dynamics authors'
 author = 'the surface-dynamics authors'
 
 # The full version, including alpha/beta/rc tags
-release = '0.4.7'
+release = '0.5.0'
 
 # -- General configuration ---------------------------------------------------
 
