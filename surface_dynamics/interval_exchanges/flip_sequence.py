d4f4c5f7e14e501a382d1d7beac0291c52e085cc
Frédéric Chapoton <chapoton@unistra.fr>
2025-05-23 14:18:02 +0200

    remove all remaining imports from __future__

--- surface_dynamics/interval_exchanges/flip_sequence.py
+++ surface_dynamics/interval_exchanges/flip_sequence.py
@@ -7,16 +7,14 @@ A concatenation of:
 - left_right_inverse, top_bottom_inverse, symmetric
 - relabeled
 """
-#*****************************************************************************
+# ***************************************************************************
 #       Copyright (C) 2021-2022 Vincent Delecroix <20100.delecroix@gmail.com>
 #
 #  Distributed under the terms of the GNU General Public License (GPL)
 #  as published by the Free Software Foundation; either version 2 of
 #  the License, or (at your option) any later version.
 #                  https://www.gnu.org/licenses/
-#*****************************************************************************
-from __future__ import print_function, absolute_import
-
+# ***************************************************************************
 import numbers
 from copy import copy
 
